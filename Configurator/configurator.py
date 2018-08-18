611fd70683c8aa14c08f60c16b9fa60a046e8cc6
Aviad <greenfighter@gmail.com>
2018-08-18 23:23:37 +0300

    Configurator linting

--- Configurator/configurator.py
+++ Configurator/configurator.py
@@ -1,10 +1,13 @@
+"""
+Linger configurator is a flask webservice that generates a Linger json config file.
+"""
 # Imports
 import os
 import sys
 from flask import Flask, request, render_template, Response
 import json
 import uuid
-
+from future.utils import iteritems
 # Adding current directory to the path
 sys.path.append(os.getcwd())
 
@@ -34,27 +37,40 @@ app.config.from_object(__name__)
 
 # Helper methods
 def get_configuration():
-    with open(CONFIG_FILE, 'r') as f:
+    linger_configuration_file_path = CONFIG_FILE
+
+    if 2 >= len(sys.argv):
+        linger_configuration_file_path = sys.argv[1]
+
+    with open(linger_configuration_file_path, 'r') as f:
         returned_branch = json.loads(f.read())
 
     return returned_branch
 
 
 def set_configuration(configuration):
-    with open(CONFIG_FILE, 'w') as fw:
+    linger_configuration_file_path = CONFIG_FILE
+
+    if 2 >= len(sys.argv):
+        linger_configuration_file_path = sys.argv[1]
+
+    with open(linger_configuration_file_path, 'w') as fw:
         fw.write(json.dumps(configuration))
 
+
 def parse_fields(fields):
-# This method should be updated in case of updating the fields system in Linger
+    # This method should be updated in case of updating the fields system in Linger
+
     parsed = {}
-    parsed["mandatory"] = [] 
-    parsed["optional"] = [] 
+    parsed["mandatory"] = []
+    parsed["optional"] = []
     for field in fields[0]:
-        parsed["mandatory"].append(field) 
+        parsed["mandatory"].append(field)
     for field in fields[1]:
         parsed["optional"].append(field)
 
     return parsed
+
 
 def get_items_structure(loading_configuration):
     managers = []
@@ -65,21 +81,23 @@ def get_items_structure(loading_configuration):
 
     for manager in managers:
         item_structure[manager.manager_type] = {}
-        for item_type, item_factory in manager.loaded_plugins_by_types.iteritems():
+        for item_type, item_factory in iteritems(manager.loaded_plugins_by_types):
             item_structure[manager.manager_type][item_type] = parse_fields(item_factory.get_fields())
 
     return item_structure
+
 
 @app.route('/')
 def conifgurator():
     return render_template('index.html', server_address=request.host)
 
+
 @app.route('/api/get_configuration', methods=['GET'])
 def get_configuration_handler():
-    configuration = {} 
+
+    configuration = {}
     configuration["Linger"] = get_configuration()
     configuration["ItemsStructure"] = get_items_structure(configuration["Linger"])
-
 
     return Response(
         json.dumps(configuration),
@@ -89,9 +107,10 @@ def get_configuration_handler():
         }
     )
 
+
 @app.route('/api/get_uuid', methods=['GET'])
 def get_uuid_for_add_handler():
-    uuid_hex = uuid.uuid4().get_hex()
+    uuid_hex = uuid.uuid4().hex
 
     return Response(
         json.dumps(uuid_hex),
@@ -101,17 +120,18 @@ def get_uuid_for_add_handler():
         }
     )
 
-@app.route('/api/upload_configuration', methods=['GET','POST'])
+@app.route('/api/upload_configuration', methods=['GET', 'POST'])
 def upload_configuration():
     error = None
     # Uploading config file
     if request.method == 'POST':
-        configuration = json.loads(request.data)
+        configuration = json.loads(request.data.decode('utf-8'))
+        # noinspection PyBroadException
         try:
             set_configuration(configuration)
-        except Exception, e:
-            app.logger.debug(e)
+        except Exception:
             error = "problem while saving configuration"
+            app.logger.error(error, exc_info=True)
 
     return render_template('upload_configuration.html', error=error)
 
@@ -122,7 +142,9 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
     return response
 
+
 if __name__ == '__main__':
     # Reloader is bugged, Doesn't work on HGFS(VMWare filesystem).
-    app.run(host='0.0.0.0', use_reloader = True, debug=True )
+    use_reloader = os.environ.get("CONFIGURATOR_USE_RELOADER", False)
+    app.run(host='0.0.0.0', use_reloader=use_reloader, debug=True)
     # app.run(host='0.0.0.0')
